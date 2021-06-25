9d628ad4bd97481dc1d07a01319d6537020bd2d8
m-pad-studios <mpadinds@gmail.com>
2021-06-25 10:04:48 -0700

    fixing heroku

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_URL = "/static/"
 
 MEDIA_URL = "/images/"
