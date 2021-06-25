751c4217cfe5e2d3069082103c59bcde15076062
m-pad-studios <mpadinds@gmail.com>
2021-06-25 11:09:38 -0700

    Update views.py

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,5 @@
 import json
-from rest_framework.response import Response
+
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from django.http import Http404, HttpResponse, JsonResponse
